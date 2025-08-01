43be068e4c440869c4b414aa12c764c532fe335b
d3ller <corentin.nelhomme@gmail.com>
2025-08-01 09:56:52 +0200

    install vitest and create utils units test

--- tests/units/Utils.test.js
+++ tests/units/Utils.test.js
@@ -1,14 +1,14 @@
 import { expect, test } from 'vitest'
-import { Utils } from '../../src'
+import { Utils } from '../../src/core/Utils'
 
-let utils = new Utils();
+const utils = new Utils();
 
 test('Encode Hello world!', () => {
-
     const encoded = utils.utoa("Hello world!");
-    console.log(encoded);
+    expect(encoded).toBe('SGVsbG8gd29ybGQh');
+});
 
-})
-
-     /* const encoded = utils.utoa("Hello world!");
-     * console.log(encoded); // "SGVsbG8gd29ybGQh"*/
+test('Decode SGVsbG8gd29ybGQh', () => {
+    const decoded = utils.atou("SGVsbG8gd29ybGQh");
+    expect(decoded).toBe('Hello world!');
+});
